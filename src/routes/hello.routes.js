4b8f95e306e7ed10b36802754364155eb6c5eef4
Karina1014 <janneth_wi15@hotmail.com>
2025-01-16 00:43:54 -0500

    feat: :white_check_mark: Update
    Update file

--- src/routes/hello.routes.js
+++ src/routes/hello.routes.js
@@ -5,7 +5,7 @@ const router = Router()
 
 router.get('/', (req, res)=>{
     const hostname = os.hostname();
-    res.send(`Container ID >: ${hostname}`)
+    res.send(`Container Id ${hostname}`)
 })
 
 export default router
